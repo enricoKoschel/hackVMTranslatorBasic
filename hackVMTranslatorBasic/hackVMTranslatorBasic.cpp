b8ba2eb8055018f1c496b317cd081d439ba3def7
enricoKoschel <enricokoschel@outlook.de>
2020-01-13 13:58:46 +0100

    added api variables and methods

--- hackVMTranslatorBasic/hackVMTranslatorBasic.cpp
+++ hackVMTranslatorBasic/hackVMTranslatorBasic.cpp
@@ -8,6 +8,81 @@ fs::path inputFilePath;
 bool directory = false;
 fs::path outputFilePath;
 
+enum class command {
+	C_ARITHMETIC,
+	C_PUSH, C_POP,
+	C_LABEL, C_GOTO,
+	C_IF,
+	C_FUNCTION,
+	C_RETURN,
+	C_CALL
+};
+
+class parserModule {
+private:
+	bool hasMoreCommands = true;
+	command commandType;
+	string arg1;
+	int arg2;
+public:
+	parserModule() {
+
+	}
+
+	void advance() {
+		if (!hasMoreCommands) return;
+	}
+
+	bool getHasMoreCommands() {
+		return hasMoreCommands;
+	}
+
+	string getArg1() {
+		if (commandType == command::C_RETURN) return "-1";
+		return arg1;
+	}
+
+	int getArg2() {
+		if (commandType == command::C_PUSH || commandType == command::C_POP 
+			|| commandType == command::C_FUNCTION || commandType == command::C_CALL) {
+
+			return arg2;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	command getCommandType() {
+		return commandType;
+	}
+};
+
+class codeWriter {
+private:
+
+public:
+	codeWriter() {
+
+	}
+
+	void setFileName(string fileName) {
+
+	}
+
+	void writeArithmetic(string command) {
+
+	}
+
+	void writePushPop(command cmd, string segment, int index) {
+
+	}
+
+	void close() {
+
+	}
+};
+
 int handleArguments(int argc, char* argv[]) {
 	if (argc == 2) {
 		inputFilePath = argv[1];
@@ -32,5 +107,8 @@ int handleArguments(int argc, char* argv[]) {
 }
 
 int main(int argc, char* argv[]) {
-	handleArguments(argc, argv);
+	if (handleArguments(argc, argv) == 1) {
+		cerr << "Usage: hackVMTranslatorBasic.exe [inputFileName.vm] or hackVMTranslatorBasic.exe [inputFileDirectory]\noutput filename is inputFileName.asm or inputFileDirectory.asm";
+		return 1;
+	}
 }
