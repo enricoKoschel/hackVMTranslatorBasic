d0eb17614a215803095a8961b56e134f92c48131
enricoKoschel <enricokoschel@outlook.de>
2020-01-26 00:30:43 +0100

    done everything except writeArithmetic lt and gt. And no pop command yet. Also no folder support

--- hackVMTranslatorBasic/hackVMTranslatorBasic.cpp
+++ hackVMTranslatorBasic/hackVMTranslatorBasic.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <filesystem>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 namespace fs = filesystem;
 
-fs::path inputFilePath;
+fs::path inputFilePathOrDir;
 bool directory = false;
 fs::path outputFilePath;
+vector<fs::path> inputFilePaths;
+
+string arithCommands[9] = {"add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"};
 
 enum class command {
 	C_ARITHMETIC,
 	C_PUSH, C_POP,
-	C_LABEL, C_GOTO,
-	C_IF,
-	C_FUNCTION,
-	C_RETURN,
-	C_CALL
+	C_LABEL, C_GOTO,	//Next Chapter
+	C_IF,				//Next Chapter
+	C_FUNCTION,			//Next Chapter
+	C_RETURN,			//Next Chapter
+	C_CALL				//Next Chapter
 };
 
 class parserModule {
@@ -24,13 +31,63 @@ private:
 	command commandType;
 	string arg1;
 	int arg2;
+	ifstream inputFile;
+	string line;
+	int currentLine;
 public:
-	parserModule() {
-
+	parserModule(fs::path _inputFilePath) {
+		inputFile = ifstream(_inputFilePath);
+		if (!inputFile.is_open()) {
+			cerr << _inputFilePath.string() << " could not be opened!";
+			exit(1);
+		}
 	}
 
 	void advance() {
 		if (!hasMoreCommands) return;
+		if (getline(inputFile, line)) {
+			currentLine++;
+			if ((line[0] == '/' && line[1] == '/') || line == "") {
+				advance();
+			}
+
+			int firstSpace = line.find_first_of(' ');
+			int secondSpace = line.substr(firstSpace + 1).find_first_of(' ');
+
+			string firstSegment = line.substr(0, firstSpace);
+
+			if (firstSegment == "push") {
+				commandType = command::C_PUSH;
+			}
+			else if (firstSegment == "pop") {
+				commandType = command::C_POP;
+			}
+			else if (find(begin(arithCommands), end(arithCommands), firstSegment) != end(arithCommands)) {
+				commandType = command::C_ARITHMETIC;
+				arg1 = firstSegment;
+			}
+
+			if (commandType != command::C_ARITHMETIC && commandType != command::C_RETURN) {
+				string secondSegment = line.substr(firstSpace + 1, secondSpace);
+				arg1 = secondSegment;
+
+				if (commandType == command::C_PUSH || commandType == command::C_POP || commandType == command::C_FUNCTION || commandType == command::C_CALL) {
+					string thirdSegment = line.substr(firstSpace + 2).substr(secondSpace);
+
+					for (auto chr : thirdSegment) {
+						if (!isdigit(chr)) {
+							cerr << "Error on line: " << currentLine << endl;
+							exit(1);
+						}
+					}
+
+					arg2 = stoi(thirdSegment);
+				}
+			}
+		}
+		else {
+			hasMoreCommands = false;
+		}
 	}
 
 	bool getHasMoreCommands() {
@@ -60,43 +117,199 @@ public:
 
 class codeWriter {
 private:
-
+	ofstream outputFile;
+	int segmentAddress;
+	int eqs, gts, lts;
 public:
-	codeWriter() {
-
+	codeWriter(fs::path _outputFilePath) {
+		outputFile = ofstream(_outputFilePath);
+		if (!outputFile.is_open()) {
+			cerr << outputFilePath.string() << " could not be created/opened!";
+			exit(1);
+		}
 	}
 
-	void setFileName(string fileName) {
-
+	void setFileName(fs::path _outputFilePath) {
+		outputFile = ofstream(_outputFilePath);
+		if (!outputFile.is_open()) {
+			cerr << outputFilePath.string() << " could not be created/opened!";
+			exit(1);
+		}
 	}
 
 	void writeArithmetic(string command) {
+		if (command == "add") {
+			outputFile << "@0" << endl;
+			outputFile << "A=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "D=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "M=D+M" << endl;
+			outputFile << "@0" << endl;
+			outputFile << "M=M-1" << endl;
+		}
+		else if (command == "sub") {
+			outputFile << "@0" << endl;
+			outputFile << "A=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "D=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "M=M-D" << endl;
+			outputFile << "@0" << endl;
+			outputFile << "M=M-1" << endl;
+		}
+		else if (command == "neg") {
+			outputFile << "@0" << endl;
+			outputFile << "A=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "M=-M" << endl;
+		}
+		else if (command == "eq") {
+			outputFile << "@0" << endl;
+			outputFile << "M=M-1" << endl;
+			outputFile << "A=M" << endl;
+			outputFile << "D=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "D=M-D" << endl;
+			outputFile << "@EQUAL" << eqs << endl;
+			outputFile << "D;JEQ" << endl;
+			outputFile << "@0" << endl;
+			outputFile << "A=M-1" << endl;
+			outputFile << "M=0" << endl;
+			outputFile << "@DONEEQUAL" << eqs << endl;
+			outputFile << "0;JMP" << endl;
+			outputFile << "(EQUAL" << eqs << ")" << endl;
+			outputFile << "@0" << endl;
+			outputFile << "A=M-1" << endl;
+			outputFile << "M=-1" << endl;
+			outputFile << "(DONEEQUAL" << eqs << ")" << endl;
+			eqs++;
+		}
+		else if (command == "gt") {
 
+		}
+		else if (command == "lt") {
+
+		}
+		else if (command == "and") {
+			outputFile << "@0" << endl;
+			outputFile << "A=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "D=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "M=M&D" << endl;
+			outputFile << "@0" << endl;
+			outputFile << "M=M-1" << endl;
+		}
+		else if (command == "or") {
+			outputFile << "@0" << endl;
+			outputFile << "A=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "D=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "M=M|D" << endl;
+			outputFile << "@0" << endl;
+			outputFile << "M=M-1" << endl;
+		}
+		else if (command == "not") {
+			outputFile << "@0" << endl;
+			outputFile << "A=M" << endl;
+			outputFile << "A=A-1" << endl;
+			outputFile << "M=!M" << endl;
+		}
 	}
 
 	void writePushPop(command cmd, string segment, int index) {
+		if (segment == "argument") {
+			segmentAddress = 2;
+		}
+		else if (segment == "local") {
+			segmentAddress = 1;
+		}
+		else if (segment == "static") {
+			segmentAddress = 15; //temporary
+		}
+		else if (segment == "constant") {
+			segmentAddress = -1;
+		}
+		else if (segment == "this") {
+			segmentAddress = 3;
+		}
+		else if (segment == "that") {
+			segmentAddress = 4;
+		}
+		else if (segment == "pointer") {
+			segmentAddress = 13; //temporary
+		}
+		else if (segment == "temp") {
+			segmentAddress = 5; //temporary
+		}
+		
+		if (cmd == command::C_PUSH) {
+			//outputFile << "@0" << endl;
+			//outputFile << "D=M" << endl;
+			if (segmentAddress > 0) {
+				if (segmentAddress == 3) {
+					outputFile << "@13" << endl;
+					outputFile << "D=M" << endl;
+					outputFile << "@3" << endl;
+					outputFile << "D=D+M" << endl;
+				}
+				else if (segmentAddress == 4) {
+					outputFile << "@14" << endl;
+					outputFile << "D=M" << endl;
+					outputFile << "@4" << endl;
+					outputFile << "D=D+M" << endl;
+				}
+				else {
+					outputFile << "@" << segmentAddress << endl;
+					outputFile << "D=A" << endl;
+				}
 
+				outputFile << "@" << index << endl;
+				outputFile << "D=D+A" << endl;
+				outputFile << "A=D" << endl;
+				outputFile << "D=M" << endl;
+				outputFile << "@0" << endl;
+				outputFile << "A=M" << endl;
+				outputFile << "M=D" << endl;
+				outputFile << "@0" << endl;
+				outputFile << "M=M+1" << endl;
+			}
+			else {
+				outputFile << "@" << index << endl;
+				outputFile << "D=A" << endl;
+				outputFile << "@0" << endl;
+				outputFile << "A=M" << endl;
+				outputFile << "M=D" << endl;
+				outputFile << "@0" << endl;
+				outputFile << "M=M+1" << endl;
+			}
+		}
+		else if (cmd == command::C_POP) {
+
+		}
 	}
 
 	void close() {
-
+		outputFile.close();
 	}
 };
 
 int handleArguments(int argc, char* argv[]) {
 	if (argc == 2) {
-		inputFilePath = argv[1];
-		int pos = inputFilePath.string().find(".vm", 1);
+		inputFilePathOrDir = argv[1];
+		int pos = inputFilePathOrDir.string().find(".vm", 1);
 		if (pos != string::npos) {
-			outputFilePath = inputFilePath.string().substr(0, pos) + ".asm";
+			outputFilePath = inputFilePathOrDir.string().substr(0, pos) + ".asm";
 		}
 		else {
-			int lastItem = inputFilePath.string().length() - 1;
-			while (inputFilePath.string()[lastItem] == '/' || inputFilePath.string()[lastItem] == '\\') {
-				inputFilePath = inputFilePath.string().substr(0, lastItem);
-				lastItem = inputFilePath.string().length() - 1;
+			int lastChar = inputFilePathOrDir.string().length() - 1;
+			while (inputFilePathOrDir.string()[lastChar] == '/' || inputFilePathOrDir.string()[lastChar] == '\\') {
+				inputFilePathOrDir = inputFilePathOrDir.string().substr(0, lastChar);
+				lastChar = inputFilePathOrDir.string().length() - 1;
 			}
-			outputFilePath = inputFilePath.string() + ".asm";
+			outputFilePath = inputFilePathOrDir.string() + ".asm";
 			directory = true;
 		}
 	}
@@ -110,5 +323,18 @@ int main(int argc, char* argv[]) {
 	if (handleArguments(argc, argv) == 1) {
 		cerr << "Usage: hackVMTranslatorBasic.exe [inputFileName.vm] or hackVMTranslatorBasic.exe [inputFileDirectory]\noutput filename is inputFileName.asm or inputFileDirectory.asm";
 		return 1;
+	}
+
+	parserModule bob = parserModule(inputFilePathOrDir);
+	codeWriter cW = codeWriter(outputFilePath);
+
+	while (bob.getHasMoreCommands()) {
+		bob.advance();
+		if (bob.getCommandType() == command::C_ARITHMETIC) {
+			cW.writeArithmetic(bob.getArg1());
+		}
+		else if (bob.getCommandType() == command::C_PUSH || bob.getCommandType() == command::C_POP) {
+			cW.writePushPop(bob.getCommandType(), bob.getArg1(), bob.getArg2());
+		}
 	}
 }
